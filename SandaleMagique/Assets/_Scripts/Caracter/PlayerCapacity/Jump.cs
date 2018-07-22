ab9fb0c19bd7d9d58f86564f335b96670576ff49
Nicolas <n.bocquet@student.isartdigital.com>
2018-07-22 16:53:21 +0200

    1st vers of new jump, opti to do

--- SandaleMagique/Assets/_Scripts/Caracter/PlayerCapacity/Jump.cs
+++ SandaleMagique/Assets/_Scripts/Caracter/PlayerCapacity/Jump.cs
@@ -5,6 +5,15 @@ public class Jump : MonoBehaviour {
     [SerializeField]
     private AnimationCurve m_jumpCurve;
 
+    [SerializeField]
+    private float m_horizontalSpeed;
+    [SerializeField]
+    private float m_gravity;
+    [SerializeField]
+    private float m_jumpMaxHeigt;
+    [SerializeField]
+    private float m_jumpAngle;
+
     private Animator m_animator = null;
     
     private float m_timer = 0f;
@@ -52,7 +61,8 @@ public class Jump : MonoBehaviour {
             return;
         }
         Vector3 pos = transform.position;
-        pos.y = m_jumpCurve.Evaluate(m_timer) + m_initY;
+        pos.y = -(m_gravity / 2) * (m_timer * m_timer) + m_horizontalSpeed * Mathf.Sin(m_jumpAngle) * m_timer + m_initY;
+        //pos.y = m_jumpCurve.Evaluate(m_timer) + m_initY;
         transform.position = pos;
         m_timer += Time.fixedDeltaTime;
 
