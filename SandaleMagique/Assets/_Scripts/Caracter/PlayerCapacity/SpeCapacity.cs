8f9fdbe18a0103777570b6c7da3e007cb0771551
Nicolas <n.bocquet@student.isartdigital.com>
2018-08-02 03:28:50 +0200

    Arret en l'air plus raycast aprtes utilisation du dash done

--- SandaleMagique/Assets/_Scripts/Caracter/PlayerCapacity/SpeCapacity.cs
+++ SandaleMagique/Assets/_Scripts/Caracter/PlayerCapacity/SpeCapacity.cs
@@ -13,6 +13,8 @@ public class SpeCapacity : MonoBehaviour
     private int m_refillRateModifier;
     [SerializeField]
     private float m_dashSpeedModifier;
+    [SerializeField]
+    private float m_waitTimeBeforeActivateDash;
 
     private InputManager m_manager;
     private Animator m_animator = null;
@@ -20,6 +22,8 @@ public class SpeCapacity : MonoBehaviour
 
     private int m_gaugeValue;
     public int GaugeValue { get { return m_gaugeValue; } }
+    private bool m_dashHasBeenUsed = false;
+    private Vector3 currentPos;
 
     private float m_characterSpeed;
 
@@ -27,6 +31,7 @@ public class SpeCapacity : MonoBehaviour
     {
         if (!m_animator.GetBool("SpeCap"))
         {
+            currentPos = transform.position;
             m_animator.SetBool("SpeCap", true);
         }
         OnDisable();
@@ -58,31 +63,56 @@ public class SpeCapacity : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 pos = transform.position;
+        if (m_animator.GetBool("SpeCap") && m_gaugeValue > 0 && !m_dashHasBeenUsed)
+        {
+            transform.position = currentPos;
+            GetComponent<Rigidbody>().useGravity = false;
+            Debug.DrawLine(pos, CalculateDashDir(pos), Color.red);
+            StartCoroutine(ActivateDash());
+        }
+        if (m_dashHasBeenUsed)
+            UseDash(pos);
+    }
+
+    private IEnumerator ActivateDash()
+    {
+        yield return new WaitForSecondsRealtime(m_waitTimeBeforeActivateDash);
+        m_dashHasBeenUsed = true;
+        yield break;
+    }
+
+    private void UseDash(Vector3 pos)
+    {
+        GetComponent<Rigidbody>().useGravity = true;
         if (m_animator.GetBool("SpeCap") && m_gaugeValue > 0)
         {
-            switch (m_cControler.HorizontalDir)
-            {
-                case E_Direction.RIGHT: pos.x += m_dashSpeedModifier * m_characterSpeed * Time.fixedDeltaTime; break;
-                case E_Direction.LEFT: pos.x -= m_dashSpeedModifier * m_characterSpeed * Time.fixedDeltaTime; break;
-            }
-
-            if (m_cControler.VerticalDir == E_Direction.UP)
-            {
-                pos.y += m_dashSpeedModifier * m_characterSpeed * Time.fixedDeltaTime;
-            }
-
-            transform.position = pos;
+            transform.position = CalculateDashDir(pos);
             m_gaugeValue -= m_consomationRatePerUse;
         }
         else if (m_animator.GetBool("SpeCap") && m_gaugeValue <= 0)
         {
-            if (m_gaugeValue < 0)
-                m_gaugeValue = 0;
-
             Reset();
             return;
         }
     }
+
+    private Vector3 CalculateDashDir(Vector3 pos)
+    {
+        switch (m_cControler.HorizontalDir)
+        {
+            case E_Direction.RIGHT: pos.x += m_dashSpeedModifier * m_characterSpeed * Time.fixedDeltaTime; break;
+            case E_Direction.LEFT: pos.x -= m_dashSpeedModifier * m_characterSpeed * Time.fixedDeltaTime; break;
+        }
+
+        if (m_cControler.VerticalDir == E_Direction.UP)
+        {
+            pos.y += m_dashSpeedModifier * m_characterSpeed * Time.fixedDeltaTime;
+        }
+
+        return pos;
+    }
+
+
     public void FillSpeCapBar(Vector3 oldPos, Vector3 currentPos)
     {
         if (oldPos.y > currentPos.y && m_gaugeValue < m_gaugeBarMaxCapacity)
